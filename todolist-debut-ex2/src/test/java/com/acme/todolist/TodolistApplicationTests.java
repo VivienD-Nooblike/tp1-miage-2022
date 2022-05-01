6c7746571995072e7df6f88b7b221857b8733f3c
CompteTemp <hugo8.hh@gmail.com>
2022-05-01 13:31:46 +0200

    exo3 tests

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,5 +1,6 @@
 package com.acme.todolist;
 
+
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -11,11 +12,24 @@ import com.acme.todolist.domain.TodoItem;
 
 @SpringBootTest
 class TodolistApplicationTests {
+	
+	private static TodoItem item;
 
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	public void test_isLate() {
+		item = new TodoItem("test1", Instant.now().minus(2, ChronoUnit.DAYS), "test1");
+		Assertions.assertTrue(item.finalContent().contains("[LATE!]"));
+	}
+	
+	@Test
+	public void test_isNotLate() {
+		item = new TodoItem("test2", Instant.now(), "test2");
+		Assertions.assertFalse(item.finalContent().contains("[LATE!]"));
+	}
 	
 
 }
